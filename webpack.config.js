a7e8a030d0d1b257b9236f1ef035af49f54911c7
bt <biantaobrad@gmail.com>
2020-08-24 14:41:15 +0800

    css loader配置完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,78 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const autoprefixer = require('autoprefixer')
+const ENV = process.env.NODE_ENV || 'development'
+const IS_PROD = ENV === 'production'
+const SOURCE_DIR = path.resolve(__dirname, 'src')
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node-modules/,
+        use: IS_PROD ? [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { minimize: true }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer({ browsers: 'last 5 versions'  })],
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                SOURCE_DIR,
+              ],
+            }
+          }
+        ] : [
+          {
+            loader: 'style-loader',
+            options: { singleton: true }
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer({ browsers: 'last 5 versions'  })],
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                SOURCE_DIR,
+              ],
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer({ browsers: 'last 5 versions' })],
+              sourceMap: true,
+            },
+          },
+        ],
+      } 
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'React App',
